b45d91a4ecd14112fc89b498cce304140f2f3871
eurohlam <eurohlam@gmail.com>
2019-07-07 18:41:19 +1200

    updated opencv from 2.5.4 to 3.4.3

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# opencv-java-sanples
+# opencv-java-samples
+
+## how to install opencv for MAC using macport
+
+    sudo port selfupdate
+    sudo port install opencv +java
+
+Check the installation
+    
+    port contents opencv | grep java
+    
+It should show 
+    
+    /opt/local/share/OpenCV/java/libopencv_java343.dylib
+    /opt/local/share/OpenCV/java/opencv-343.jar
+    
+## how to add opencv support for java application
+    
+- alternative 1: add libopencv_java343.dylib as Java extension in MAC
+ 
+        cp /opt/local/share/OpenCV/java/libopencv_java343.dylib /Library/Java/Extensions
+    
+- alternative 2: run application with java.library.path
+    
+        java -Djava.library.path=/opt/local/share/OpenCV/java
--- src/main/java/org/roag/Threshold.java
+++ src/main/java/org/roag/Threshold.java
@@ -1,10 +1,8 @@
 package org.roag;
 
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 
-import org.opencv.highgui.Highgui;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
 import java.util.Date;
@@ -23,13 +21,14 @@ public class Threshold
         {
             long starttime=new Date().getTime();
             System.out.println("Start time "+0);
+            System.setProperty("java.library.path", "/opt/local/share/OpenCV/java");
             System.loadLibrary(org.opencv.core.Core.NATIVE_LIBRARY_NAME);
-            Mat source = Highgui.imread("salmon_spot.jpg", Highgui.CV_LOAD_IMAGE_COLOR);
+            Mat source = Imgcodecs.imread("salmon_spot.jpg", Imgcodecs.CV_LOAD_IMAGE_COLOR);
             Mat destination = new Mat(source.rows(), source.cols(), source.type());
 
             destination = source;
-            Imgproc.threshold(source, destination, 180, 255, Imgproc.THRESH_TRUNC);
-            Highgui.imwrite("salmon_threshold.jpg", destination);
+            Imgproc.threshold(source, destination, 100, 255, Imgproc.THRESH_TRUNC);
+            Imgcodecs.imwrite("salmon_threshold.jpg", destination);
 
             long end_time=new Date().getTime();
             System.out.println("End time "+(end_time-starttime));
